4e71b2f9f56ba38cc62606147321ce538918d583
qaruda <70809588+francistinao@users.noreply.github.com>
2023-10-07 21:59:05 +0800

    .

--- src/pages/Jobs.tsx
+++ src/pages/Jobs.tsx
@@ -61,7 +61,7 @@ function SetOfJobs({ name, desc }: { name: string; desc: string }): ReactNode {
 
 function Jobs() {
 	return (
-		<div className='bg-main h-screen'>
+		<div className='bg-main w-full pb-36'>
 			<div className='flex flex-col gap-4 w-full mb-10 container'>
 				<h1 className='font-span text-black mt-4 font-extrabold xxxs:text-2xl w-11/12 lg:text-4xl w-9/12 xl:text-6xl 2xl:text-7xl w-5/12'>
 					Work Experience
